7e00f73fe94eea1c2513d6d10d281c32c3268cfc
XNAL <772528797@qq.com>
2019-02-26 21:56:08 +0800

    tabbar添加icon

--- src/navigator/index.js
+++ src/navigator/index.js
@@ -1,8 +1,10 @@
+import React from 'react'
 import { createBottomTabNavigator, createStackNavigator, createAppContainer } from 'react-navigation'
+import Entypo from 'react-native-vector-icons/Entypo'
 
-import HomeScreen from '../pages/home'
-import LaboratoryScreen from '../pages/laboratory'
-import AboutScreen from '../pages/about'
+import HomeScreen from '../screen/home'
+import LaboratoryScreen from '../screen/laboratory'
+import AboutScreen from '../screen/about'
 
 // tab导航
 const TabNavigator = createBottomTabNavigator(
@@ -10,24 +12,47 @@ const TabNavigator = createBottomTabNavigator(
     Home: {
       screen: HomeScreen,
       navigationOptions: {
-        tabBarLabel: '首页'
+        tabBarLabel: '首页',
+        tabBarIcon: ({ tintColor }) => (
+          <Entypo name={ 'home' } size={ 24 } color={ tintColor } />
+        )
       }
     },
     Laboratory: {
       screen: LaboratoryScreen,
       navigationOptions: {
         tabBarLabel: '实验室',
+        tabBarIcon: ({ tintColor }) => (
+          <Entypo name={ 'code' } size={ 24 } color={ tintColor } />
+        )
       }
     },
     About: {
       screen: AboutScreen,
       navigationOptions: {
         tabBarLabel: '我的',
+        tabBarIcon: ({ tintColor }) => (
+          <Entypo name={ 'user' } size={ 24 } color={ tintColor } />
+        )
       }
     }
   },
   {
-    initialRouteName: 'Home'
+    initialRouteName: 'Home',
+    tabBarOptions: {
+      activeTintColor: '#2d8cf0', // 选中的颜色
+      inactiveTintColor: '#8F8F8F', // 未选中的颜色
+      style: {
+        backgroundColor: '#f3f3f3',
+        height: 54
+      },
+      tabStyle: {
+        height: 54
+      },
+      labelStyle: {
+        fontSize: 14
+      }
+    }
   }
 )
 
